1bb3913c3ce715b19e416d44ba738c985e2366c2
M1guel-cb <132688685+M1guel-cb@users.noreply.github.com>
2024-01-03 14:11:46 -0300

    cansei

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,5 @@
-const btn = document.querySelector('#btn');
-const bola = document.querySelector('#bola');
-let tema_sitema = localStorage.getItem('theme') || 'light';
+const btn = document.querySelector("form button")
 
-btn.addEventListener('click', () => {
-    let oldTema = localStorage.getItem('theme') || 'light';
-    let newTema = oldTema == 'light' ? 'dark' : 'light';
-    localStorage.setItem('theme', newTema);
-    temaAtual(newTema);
-});
-
-function temaAtual(theme) {
-    document.documentElement.setAttribute('data-theme', theme)
-    if (theme == 'light') {
-        bola.style.transform = 'translateX(28px)';
-    } else {
-        bola.style.transform = 'translateX(0)';
-    }
-}
-
-temaAtual(tema_sitema);
+btn.addEventListener('click', (e) => {
+    e.preventDefault()
+})
