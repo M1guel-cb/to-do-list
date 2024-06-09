fc78cd01da6796b44ef03d9ae7da30bf09862e2b
M1guel-cb <132688685+M1guel-cb@users.noreply.github.com>
2024-06-09 14:03:57 -0300

    foi ein

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,54 @@
-const btn = document.querySelector("form button")
+const btn = document.querySelector("form button");
+const input = document.querySelector("#task-input");
+const ul = document.querySelector("#ul-task");
+let api = JSON.parse(localStorage.getItem("to-do-list-m1guelcb") || "[]");
 
-btn.addEventListener('click', (e) => {
-    e.preventDefault()
-})
+btn.addEventListener("click", (e) => {
+    e.preventDefault();
+    if (!input.value) {
+        alert("Digite algo para inserir em sua lista!");
+        return
+    } else if (validar(input.value)) {
+        alert("Valor já existente no TO-DO!");
+        return
+    } else {
+        setarValores(input.value);
+        mostrarValores();
+        input.value = "";
+    }
+});
+
+function validar(data) {
+    for (task in api) {
+        if (api[task].nome == data) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function setarValores(task) {
+    api.push({ nome: task });
+    localStorage.setItem("to-do-list-m1guelcb", JSON.stringify(api));
+}
+
+function mostrarValores() {
+    ul.innerHTML = "";
+    for (item in api) {
+        ul.innerHTML += `<li class="task">
+                    <h2>${api[item].nome}</h2>
+                    <div>
+                        <button class="check" onclick=deleteValor("${api[item].nome}")><i class="fa-regular fa-circle-check"></i></button>
+                    </div>
+                </li>`;
+    }
+}
+
+function deleteValor(data) {
+    let index = api.findIndex((x) => x.name == data);
+    api.splice(index, 1);
+    localStorage.setItem("to-do-list-m1guelcb", JSON.stringify(api));
+    mostrarValores();
+}
+
+mostrarValores();
